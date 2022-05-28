be39b02f326a592786c2b2a3cbd2a038f2ff56e4
Veronika Vashkevich <veronik.vashkevich@gmail.com>
2022-05-28 18:42:47 +0300

    favicon, pages titles

--- resources/views/templates.blade.php
+++ resources/views/templates.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/app')
 
-@section('title', 'Меню')
+@section('title', 'Документы')
 
 @section('header')
     @parent
--- resources/views/templates/form1.blade.php
+++ resources/views/templates/form1.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/app')
 
-@section('title', 'Меню')
+@section('title', 'Создать документ')
 
 @section('header')
     @parent
